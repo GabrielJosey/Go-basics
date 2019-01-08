e70303e3e0d73be2448b983d258569df66ae97df
Gabriel <Acts2024Life@gmail.com>
2019-01-08 14:50:04 -0500

    types added

---
+++ types.go
@@ -0,0 +1,9 @@
+/*
+1) int, int8, int16, int32, int64   The lower the int# the smaller a number you can keep stored.
+int# can store both positive and negative numbers
+2)uint, uint8, uint16, uint32, uint64     uint# can only hold positive numbers
+unless you have a specific reason to use uint# then its probably not worth using
+3) float32, float64     for floating point numbers, that is numbers with a decimal point
+you will want to use the float32 or float64 types
+*/
+package main
