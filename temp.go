a845172f1154b449a7bccf12bfd31b26e734d699
Gabriel <Acts2024Life@gmail.com>
2019-01-08 13:34:36 -0500

    adding more notes

--- temp.go
+++ temp.go
@@ -4,3 +4,12 @@ import (
 	"fmt"
 	"math"
 )
+
+var myNumber = 1.23
+
+func main() {
+
+	roundedUp := math.Ceil(myNumber)
+	roundedDown := math.Floor(myNumber)
+	fmt.Println(roundedUp, roundedDown)
+}
